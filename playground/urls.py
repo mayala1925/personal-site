ae917ecd668ffcae07766781de962dbec2f68206
Matt Ayala <matthewayala@Matts-MacBook-Pro.local>
2021-11-01 11:26:45 -0600

    first commit, add files

---
+++ playground/urls.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 10:37:32 2021
+
+@author: matthewayala
+"""
+
+from django.urls import path
+from . import views
+
+# URLconf
+urlpatterns = [
+    path('hello/', views.say_hello),
+    path('hello/', views.home)
+    
+    ]
